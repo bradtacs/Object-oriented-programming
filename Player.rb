16b1fdabd5d9f84537ced961342719ef242ee31b
Zachary Viggiani <zach@bigmojo.net>
2017-04-01 15:42:27 -0400

    Close still need to add lives

--- Player.rb
+++ Player.rb
@@ -1,11 +1,26 @@
 class Player
-  attr_reader :gold_goins :health_points :lives
+  attr_reader :gold_goins, :health_points, :lives
 
   def initialize
     @gold_goins = 0
     @health_points = 10
     @lives = 5
 
+  end
+  def level_up
+     @lives += 1
+  end
+
+  def collect_treasure
+    if @gold_goins > 10
+      @gold_goins += 1
+     end
+  end
+
+  def restart
+    @gold_goins = 0
+    @health_points = 10
+    @lives = 5
   end
 
 end

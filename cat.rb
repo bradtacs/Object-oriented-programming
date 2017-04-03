fd081b68990091d0b1f687c369673b39082e38fa
Zachary Viggiani <zach@bigmojo.net>
2017-04-03 14:51:21 -0400

    Done

--- cat.rb
+++ cat.rb
@@ -1,5 +1,5 @@
 class Cat
-  attr_reader :name, :preferred_food, :meal_time
+  attr_accessor :name, :preferred_food, :meal_time
 
   # Means you need this information.
   def initialize(name, preferred_food, meal_time)
@@ -10,23 +10,29 @@ class Cat
 
   def eats_at
 
-    #  puts rand(24) random number
+
     if @meal_time < 12
+
       "#{@meal_time} AM"
     else
-    "#{@meal_time} PM"
-    end
 
-    def meow
-      "Hi my name is #{@name} and I eat #{@preferred_food} at #{@meal_time}!"
+      "#{@meal_time} PM"
     end
   end
+
+  def meow
+    "Hi my name is #{@name} and I eat #{@preferred_food} at #{@meal_time}!"
+  end
+
 end
 
 sam = Cat.new("Sam" , "tuna" , 21 )
 sandra = Cat.new("Sandra" , "milk" , 3)
 
-puts Sandra.eats_at
-puts Sam.eats_at
-puts Sandra.meow
-puts Sam.meow
+
+# match case name above!
+
+puts sandra.eats_at
+puts sam.eats_at
+puts sandra.meow
+puts sam.meow
